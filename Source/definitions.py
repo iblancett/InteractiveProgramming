42abeb28dbd088416e1eaa362233cca02c1c6427
Isabel Blancett <isabel.blancett@students.olin.edu>
2017-10-19 15:08:09 -0400

    progress on algos -- nothing working yet

--- Source/definitions.py
+++ Source/definitions.py
@@ -1,19 +1,23 @@
-class course(object):
+class Course(object):
     """Course option
     Contains: description for graphics, max level, order, and current level"""
-    def __init__(self, info, maxLvl):
+    def __init__(self, name, info, maxLvl):
+        self.name = name
         self.info = info
         self.max = maxLvl
+        self.reqs = [0]*maxLvl
         self.order = 0
         self.lvl = 0
     def __repr__(self):
         return '({}, {}, {})'.format(self.info, self.lvl, self.order)
-
-class reqs(object):
-    """Requirements for course to level
-    Contains: all dependencies"""
-    def __init__(self, name):
-        self.name = name
-        self.reqlist = []
-    def addreqs(self, reqlist):
-        self.reqlist = reqlist
+    def addreqs(self, effectLvl, cause, causeLvl):
+        print(self.reqs)
+        if self.reqs[effectLvl-1]:
+            self.reqs[effectLvl-1].extend([cause, causeLvl])
+        else:
+            self.reqs[effectLvl-1] = [cause, causeLvl]
+    def addorder(self, roundNumber):
+        self.order = roundNumber
+        self.lvl = 1
+    def levelup(self):
+        self.lvl = self.lvl + 1

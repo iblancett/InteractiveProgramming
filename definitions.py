2c87b85aabd78ecff279024ae1c7008c6edc62a8
sokuno222 <siena.okuno@students.olin.edu>
2017-10-18 20:26:21 -0400

    Added class defintions for course and req. Req is incomplete.

---
+++ definitions.py
@@ -0,0 +1,19 @@
+class course(object):
+    """Course option
+    Contains: description for graphics, max level, order, and current level"""
+    def __init__(self, info, maxLvl):
+        self.info = info
+        self.max = maxLvl
+        self.order = 0
+        self.lvl = 0
+    def __repr__(self):
+        return '({}, {}, {})'.format(self.info, self.lvl, self.order)
+
+class reqs(object):
+    """Requirements for course to level
+    Contains: all dependencies"""
+    def __init__(self, name):
+        self.name = name
+        self.reqlist = []
+    def addreqs(self, reqlist):
+        self.reqlist = reqlist

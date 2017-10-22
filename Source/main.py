d0b42b6bc2e528291f43a14e8e0687fa9bb4cb0d
Isabel Blancett <isabel.blancett@students.olin.edu>
2017-10-22 19:05:07 -0400

    Something didn't push

--- Source/main.py
+++ Source/main.py
@@ -1,1 +1,20 @@
-# 
+# import all scripts here
+import courses
+
+def setup():
+
+    courses = catalog.populate()
+
+    portfolio = portfolio.Portfolio()
+
+    gameplay = gameplay.Play(courses, portfolio)
+
+    return gameplay
+
+gameplay = setup()
+
+while gameplay.victory != True:
+    action = gameplay.prompt()
+    gameplay.parse(action)
+
+gameplay.victory()
